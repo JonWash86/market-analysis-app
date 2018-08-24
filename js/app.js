1c680b61bdb8d3da10d03066109fe1d1e27729e8
Jon Washington <jonathan.washington@gmail.com>
2018-08-23 19:29:00 -0700

    managed to add some instructions as well as updating prompts letting the user know their status. Began table-building loop.

--- js/app.js
+++ js/app.js
@@ -5,17 +5,15 @@ function addImages() {
   container.innerText='';
   image.setAttribute('src', 'img/' + images[0].fileName);
   image.addEventListener('click', voteMade);
-  image.addEventListener('click', tallyVote);
   container.appendChild(image);
   var image = document.createElement('img');
   image.setAttribute('src', 'img/' + images[1].fileName);
   image.addEventListener('click', voteMade);
-  image.addEventListener('click', tallyVote);
   container.appendChild(image);
   var image = document.createElement('img');
   image.setAttribute('src', 'img/' + images[2].fileName);
   image.addEventListener('click', voteMade);
-  image.addEventListener('click', tallyVote);
+  // image.addEventListener('click', relayStatus);
   container.appendChild(image);
   totalClicks += 1;
   console.log('you have clicked ' + totalClicks + ' times.');
@@ -23,26 +21,27 @@ function addImages() {
 
 var totalClicks = -1;
 
-var productImage = function(fileName) {
+var productImage = function(fileName, productName) {
   this.fileName = fileName;
+  this.productName = productName;
   this.totalVotes = 0;
 };
 
 var images = [];
-images.push(new productImage('bag.jpg'));
-images.push(new productImage('banana.jpg'));
-images.push(new productImage('boots.jpg'));
-images.push(new productImage('chair.jpg'));
-images.push(new productImage('cthulhu.jpg'));
-images.push(new productImage('dragon.jpg'));
-images.push(new productImage('pen.jpg'));
-images.push(new productImage('scissors.jpg'));
-images.push(new productImage('shark.jpg'));
-images.push(new productImage('sweep.jpg'));
-images.push(new productImage('unicorn.jpg'));
-images.push(new productImage('usb.jpg'));
-images.push(new productImage('water_can.jpg'));
-images.push(new productImage('wine_glass.jpg'));
+images.push(new productImage('bag.jpg', 'R2DBag'));
+images.push(new productImage('banana.jpg', 'Banana Sectioner'));
+images.push(new productImage('boots.jpg', 'Useless Boots'));
+images.push(new productImage('chair.jpg', 'The Chair'));
+images.push(new productImage('cthulhu.jpg', 'Cthulhu'));
+images.push(new productImage('dragon.jpg', 'Dragon Meat'));
+images.push(new productImage('pen.jpg', 'Writing Utensils'));
+images.push(new productImage('scissors.jpg', 'Multipurpose Scissors'));
+images.push(new productImage('shark.jpg', 'Jaws Bag'));
+images.push(new productImage('sweep.jpg', 'Swiffer Jr.'));
+images.push(new productImage('unicorn.jpg', 'Unicorn Meat'));
+images.push(new productImage('usb.jpg', 'Undersea USB'));
+images.push(new productImage('water_can.jpg', 'Self-Watering Can'));
+images.push(new productImage('wine_glass.jpg', 'Wine Globe'));
 
 function generateRandom(min, max){
   return Math.floor((Math.random() * (max - min)) + min);
@@ -62,12 +61,8 @@ function shuffle(array){
 
 window.addEventListener('load', addImages);
 
-function tallyVote(productImage){
-  this.totalVotes += 1;
-  console.log('image clicked'); //+ imageattribute);
-}
-
 function voteMade(event){
+  this.totalVotes += 1;
   var winner = event.target.src;
   var winnerName = winner.substring(winner.lastIndexOf('/') +1);
   console.log(winnerName);
@@ -79,13 +74,48 @@ function voteMade(event){
     };
   };
   var prompt = document.getElementById('prompt');
-  if (totalClicks < 15){
-    addImages();
-  };
-  if (totalClicks == 15){
+  if (totalClicks == 14){
+    console.log('it\'s over!')
     var container = document.getElementById('image-container');
-    container.innerText='You did it!';//this will be replaced with the table listing votes.
+    container.innerText='';
     var table = document.createElement('TABLE');
     var heading = document.createElement('th');
-  };
+    table.setAttribute('class', 'resultsTable');
+    heading.innerText = 'Results!'
+    heading.setAttribute('colspan', '2');
+    table.appendChild(heading);
+    container.appendChild(table);
+    for (var index = 0; index < images.fileName; index++){
+      var cell = document.createElement('td');
+      var row = document.createElement('tr');
+      var product = images[i].productName;
+      cell.innerText = product;
+      row.appendChild(cell);
+      cell = document.createElement('td');
+      var votes = images[i].totalVotes;
+      cell.innerText = votes;
+      row.appendChild(cell);
+      table.appendChild(row);
+    };
+  }
+  else {
+    addImages();
+  }
+  relayStatus();
 }
+
+function relayStatus(){
+  var status = document.getElementById('prompt');
+  if (totalClicks == 14){
+    status.innerText = 'This is your last vote. Don\'t blow it!'
+  }
+  else if (totalClicks < 15 && totalClicks >= 10){
+    status.innerText = 'Great! You have ' + (15 - totalClicks) + ' votes left. Use them wisely.';
+  }
+  else if (totalClicks > 0 && totalClicks < 15){
+    status.innerText = totalClicks + ' votes made and ' + (15 - totalClicks) + ' to go. Keep it up!';
+  }
+  else {
+    status.InnerText = 'You\'ve done all the voting there is to do. Give yourself a pat on the back.';
+  };
+};
